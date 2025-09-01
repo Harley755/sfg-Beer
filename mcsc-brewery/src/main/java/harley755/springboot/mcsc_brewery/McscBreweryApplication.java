38035d299d90cabc280d68a14dca42ce6bb4977b
harley <bricegoudalo@gmail.com>
2025-09-01 12:28:44 +0100

    first commit

---
+++ mcsc-brewery/src/main/java/harley755/springboot/mcsc_brewery/McscBreweryApplication.java
@@ -0,0 +1,13 @@
+package harley755.springboot.mcsc_brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McscBreweryApplication.class, args);
+	}
+
+}
---
+++ mcsc-brewery/src/test/java/harley755/springboot/mcsc_brewery/McscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package harley755.springboot.mcsc_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
