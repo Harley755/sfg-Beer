17ab565c5b3598e1c5878dd0650215bd123503a1
harley <bricegoudalo@gmail.com>
2025-09-03 11:28:27 +0100

    add service

--- mssc-beer-service/src/main/java/harley755/spring_boot/mssc_beer_service/service/BeerService.java
+++ mssc-beer-service/src/main/java/harley755/spring_boot/mssc_beer_service/service/BeerService.java
@@ -1,8 +1,22 @@
 package harley755.spring_boot.mssc_beer_service.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+
+import harley755.spring_boot.mssc_beer_service.model.BeerDto;
 
 @Service
 public class BeerService {
+
+    public void getBeerById(UUID id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBeerById'");
+    }
+
+    public BeerDto createBeer(BeerDto beerDto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createBeer'");
+    }
     
 }
